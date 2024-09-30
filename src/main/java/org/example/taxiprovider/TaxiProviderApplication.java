2eadde9d78423acb2d2524b0ddb0798ee95468a3
loziuk <loziukvika10@gmail.com>
2024-09-30 22:47:26 +0300

    Initial commit

---
+++ src/main/java/org/example/taxiprovider/TaxiProviderApplication.java
@@ -0,0 +1,13 @@
+package org.example.taxiprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiProviderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/taxiprovider/TaxiProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.taxiprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
