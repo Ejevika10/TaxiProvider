2bc35a46a40f5ffed25f9b5b1e490f4bbd678f21
Ejevika <loziukvika10@gmail.com>
2024-10-07 15:43:43 +0300

    feat: add error handling classes

---
+++ passengerService/src/main/java/com/modsen/passengerservice/exception/ExceptionApiHandler.java
@@ -0,0 +1,24 @@
+package com.modsen.passengerservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionApiHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorMessage> notFoundException(NotFoundException exception) {
+        return new ResponseEntity<>(new ErrorMessage(exception.getErrorCode(), exception.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateFieldException.class)
+    public ResponseEntity<ErrorMessage> duplicateFieldException(DuplicateFieldException exception) {
+        return new ResponseEntity<>(new ErrorMessage(exception.getErrorCode(), exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> defException(Exception exception) {
+        return new ResponseEntity<>(new ErrorMessage(500L, exception.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
