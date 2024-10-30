2327d1951c60c93aadddb7f2cb4b7a35d3531dc3
Ejevika <loziukvika10@gmail.com>
2024-10-30 15:27:16 +0300

    feat: add service names to feign clients

--- ratingService/src/main/java/com/modsen/ratingservice/client/ride/RideClient.java
+++ ratingService/src/main/java/com/modsen/ratingservice/client/ride/RideClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${ride.client.name}", url = "${ride.client.url}")
+@FeignClient(name = "${ride.client.name}", path = "${ride.client.path}")
 public interface RideClient {
     @GetMapping("/{id}")
     RideResponseDto getRideById(@PathVariable("id") long id);
