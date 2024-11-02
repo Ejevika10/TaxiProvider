0c1f6e3273225d50613d3d07cc9e3b481048ec8e
Ejevika <loziukvika10@gmail.com>
2024-11-02 14:49:39 +0300

    fix: remove unnecessary fallback methods, add ignore-exceptions for retry, add resolving of error code for client exception

--- ratingService/src/main/java/com/modsen/ratingservice/client/ExternalErrorDecoder.java
+++ ratingService/src/main/java/com/modsen/ratingservice/client/ExternalErrorDecoder.java
@@ -3,22 +3,29 @@ package com.modsen.ratingservice.client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.modsen.ratingservice.exception.ClientException;
 import com.modsen.ratingservice.exception.ErrorMessage;
+import com.modsen.ratingservice.exception.ServiceUnavailableException;
+import com.modsen.ratingservice.util.AppConstants;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 
 @Component
+@Slf4j
 public class ExternalErrorDecoder implements ErrorDecoder {
     @Override
     public Exception decode(String errorString, Response response) {
         try (InputStream bodyIs = response.body().asInputStream()) {
             ObjectMapper mapper = new ObjectMapper();
             ErrorMessage errorMessage = mapper.readValue(bodyIs, ErrorMessage.class);
+            log.info("ClientException");
             return new ClientException(errorMessage);
         } catch (Exception e) {
-            return new Exception(e.getMessage());
+            log.info("Exception");
+            log.info(e.getMessage());
+            return new ServiceUnavailableException(AppConstants.SERVICE_UNAVAILABLE);
         }
     }
 }
--- rideService/src/main/java/com/modsen/rideservice/client/ExternalErrorDecoder.java
+++ rideService/src/main/java/com/modsen/rideservice/client/ExternalErrorDecoder.java
@@ -3,13 +3,17 @@ package com.modsen.rideservice.client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.modsen.rideservice.exception.ClientException;
 import com.modsen.rideservice.exception.ErrorMessage;
+import com.modsen.rideservice.exception.ServiceUnavailableException;
+import com.modsen.rideservice.util.AppConstants;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 
 @Component
+@Slf4j
 public class ExternalErrorDecoder implements ErrorDecoder {
 
     @Override
@@ -17,9 +21,12 @@ public class ExternalErrorDecoder implements ErrorDecoder {
         try (InputStream bodyIs = response.body().asInputStream()) {
             ObjectMapper mapper = new ObjectMapper();
             ErrorMessage errorMessage = mapper.readValue(bodyIs, ErrorMessage.class);
+            log.info("ClientException");
             return new ClientException(errorMessage);
         } catch (Exception e) {
-            return new Exception(e.getMessage());
+            log.info("Exception");
+            log.info(e.getMessage());
+            return new ServiceUnavailableException(AppConstants.SERVICE_UNAVAILABLE);
         }
     }
 }
