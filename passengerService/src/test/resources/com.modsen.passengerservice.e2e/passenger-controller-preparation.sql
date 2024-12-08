a05bad18268eaa322485b0e05cdb2484eca6ed57
Ejevika <loziukvika10@gmail.com>
2024-12-09 01:35:51 +0300

    fix: delete unnecessary files

--- passengerService/src/test/resources/com.modsen.passengerservice.e2e/passenger-controller-preparation.sql
+++
@@ -1,3 +0,0 @@
-TRUNCATE TABLE passenger;
-ALTER SEQUENCE passenger_id_seq RESTART WITH 1;
-INSERT INTO passenger (id, name, email, phone, rating, deleted) VALUES (nextval('passenger_id_seq'), 'passenger', 'passenger@mail.ru', '71234567890', '0.0', 'False');
--- rideService/src/test/resources/com/modsen/rideservice/e2e/ride-controller-preparation.sql
+++
@@ -1,3 +0,0 @@
-TRUNCATE TABLE ride;
-ALTER SEQUENCE ride_id_seq RESTART WITH 1;
-INSERT INTO ride (id, driver_id, passenger_id, source_address, destination_address, ride_state, ride_date_time, ride_cost) VALUES (nextval('ride_id_seq'), 1, 1, 'Source address', 'Destination address', 'CREATED', '2024-11-24 19:00', '1000')
