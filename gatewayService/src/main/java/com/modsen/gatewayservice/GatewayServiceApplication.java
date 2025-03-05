78f186bf1c9bce92ce75abeeabb698ab1bfd20f5
Ejevika <loziukvika10@gmail.com>
2025-03-05 03:01:33 +0300

    feat: configure gateway

--- gatewayService/src/main/java/com/modsen/gatewayservice/GatewayServiceApplication.java
+++ gatewayService/src/main/java/com/modsen/gatewayservice/GatewayServiceApplication.java
@@ -1,11 +1,14 @@
 package com.modsen.gatewayservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gateway service", version = "1.0", description = "Documentation API Gateway v1.0"))
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
---
+++ gatewayService/src/main/java/com/modsen/gatewayservice/configuration/GatewayConfiguration.java
@@ -0,0 +1,22 @@
+package com.modsen.gatewayservice.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class GatewayConfiguration {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/auth-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://auth-service"))
+                .route(r -> r.path("/driver-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://driver-service"))
+                .route(r -> r.path("/passenger-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://passenger-service"))
+                .route(r -> r.path("/ride-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://ride-service"))
+                .route(r -> r.path("/rating-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://rating-service"))
+                .build();
+    }
+}
